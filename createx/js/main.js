04204b792961b2c0f396f7da7fd139be8513df30
Kirillmischenk <122627699+Kirillmischenk@users.noreply.github.com>
2023-02-10 00:14:05 +0300

    Add files via upload

---
+++ createx/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    var mixer = mixitup('.directions-list');
+
+    $('.directions-fillter_btn').on('click', function () {
+    $('.directions-fillter_btn').removeClass('directions-fillter_btn--active')
+    $(this).addClass('directions-fillter_btn--active')
+    })
+
+
+    $('.team_slider').slick({
+
+    })
+})
